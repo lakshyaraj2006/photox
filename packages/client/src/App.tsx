4da788ab7edb9eda2cd852a4662a0d07284ccd1d
Lakshyaraj Dash <189126809+lakshyaraj2006@users.noreply.github.com>
2025-07-14 10:56:40 +0530

    Initialize tailwind & shadcn/ui

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,6 +1,8 @@
 function App() {
   return (
-    <h1>PhotoX</h1>
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">PhotoX</h1>
+    </div>
   )
 }
 
